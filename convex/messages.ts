b313dcb67beca4245ec33a938c8ed4bb4f5376f4
Lillian Ellison <tehlily@gmail.com>
2024-09-14 16:12:52 -0500

    Convex Quickstart chat app with ai

--- convex/messages.ts
+++ convex/messages.ts
@@ -1,20 +1,53 @@
 import { query, mutation } from "./_generated/server";
 import { v } from "convex/values";
+import { api } from "./_generated/api";
 
 export const list = query({
   args: {},
   handler: async (ctx) => {
     // Grab the most recent messages.
     const messages = await ctx.db.query("messages").order("desc").take(100);
+    const messagesWithLikes = await Promise.all(
+      messages.map(async (message) => {
+        const likes = await ctx.db
+        .query("likes")
+        .withIndex("byMessageId", (q) => q.eq("messageId", message._id))
+        .collect();
+        return {
+          ...message,
+          likes: likes.length,
+        }
+      })
+    )
     // Reverse the list so that it's in a chronological order.
-    return messages.reverse();
+    return messagesWithLikes.reverse().map((message) => ({
+      ...message,
+      body: message.body.replaceAll(":)", "😊"),
+    }));
   },
 });
 
 export const send = mutation({
   args: { body: v.string(), author: v.string() },
-  handler: async (ctx, { body, author }) => {
+  handler: async (ctx, args) => {
+    const { body, author } = args;
     // Send a new message.
     await ctx.db.insert("messages", { body, author });
+
+    if (body.startsWith("@ai") && author !== "AI") {
+      await ctx.scheduler.runAfter(0, api.ai.chat, {
+        messageBody: body,
+      })
+    }
+  },
+});
+
+export const like = mutation({
+  args: { liker: v.string(), messageId: v.id("messages") },
+  handler: async (ctx, args) => {
+    await ctx.db.insert("likes", {
+      liker: args.liker,
+      messageId: args.messageId,
+    });
   },
 });
